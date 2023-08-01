2620b75a9e6d8590b0d228c2490bdccf7218d1c5
Reece Crook <reececrook16@gmail.com>
2023-08-01 17:45:23 -0600

    Completed Lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    i = 0
+    fibonacci_list = []
+    while i < length:
+        if i == 0 or i == 1:
+            fibonacci_list.append(i)
+            i += 1
+        else:
+            j = i - 2
+            k = i - 1
+            new_number = fibonacci_list[j] + fibonacci_list[k]
+            fibonacci_list.append(new_number)
+            i += 1
+    print(fibonacci_list)
